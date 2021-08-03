f701268c1afb61951115fc0c697275509e424466
atirson <fabiano.oliveira2213@gmail.com>
2021-08-03 16:09:43 -0300

    Initial Commit

---
+++ navbar/webpack.config.js
@@ -0,0 +1,50 @@
+const singleSpaDefaults = require("webpack-config-single-spa-react");
+const { mergeWithRules } = require("webpack-merge");
+
+const mergeRulesWithMatchingTest = mergeWithRules({
+  module: {
+    rules: {
+      test: "match",
+      use: {
+        loader: "match",
+        options: "replace",
+      },
+    },
+  },
+});
+
+module.exports = (webpackConfigEnv) => {
+  const defaultConfig = singleSpaDefaults({
+    orgName: "filoxo",
+    projectName: "navbar",
+    webpackConfigEnv,
+  });
+
+  const config = mergeRulesWithMatchingTest(defaultConfig, {
+    module: {
+      rules: [
+        {
+          test: /\.css$/i,
+          use: [
+            {
+              loader: require.resolve("style-loader"),
+              options: {
+                modules: {
+                  namedExport: false,
+                },
+              },
+            },
+            {
+              loader: require.resolve("css-loader"),
+              options: {
+                modules: true,
+              },
+            },
+          ],
+        },
+      ],
+    },
+  });
+
+  return config;
+};
